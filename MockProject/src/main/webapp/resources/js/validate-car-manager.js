83461b3f33eae390831f459e6ba6114431b56ba5
TuanNQ31 <quangtuan06052000@gmail.com>
2021-11-24 09:22:18 +0000

    Update validate-car-manager.js

--- MockProject/src/main/webapp/resources/js/validate-car-manager.js
+++ MockProject/src/main/webapp/resources/js/validate-car-manager.js
@@ -2,12 +2,6 @@ function clickAdd() {
 	if (!checkLicensePlate()) {
 		alert('License plate is invalid');
 	}
-	if (!checkCarType()) {
-		alert('Car type is invalid');
-	}
-	if (!checkCarColor()) {
-		alert('Car color is invalid');
-	}
 	else {
 		document.getElementById('form-add-car').submit();
 	}
@@ -26,32 +20,6 @@ function checkLicensePlate() {
 	return true;
 }
 
-function checkCarType() {
-	var test = document.getElementById('carType').value;
-	if (test == '') {
-		return false;
-	}
-	for (var i = 0; i < test.length; i++) {
-		if (test.charCodeAt(i) < 65 || (test.charCodeAt(i) > 90 && test.charCodeAt(i) < 97) || test.charCodeAt(i) > 122) {
-			return false;
-		}
-	}
-	return true;
-}
-
-function checkCarColor() {
-	var test = document.getElementById('carColor').value;
-	if (test == '') {
-		return false;
-	}
-	for (var i = 0; i < test.length; i++) {
-		if (test.charCodeAt(i) < 65 || (test.charCodeAt(i) > 90 && test.charCodeAt(i) < 97) || test.charCodeAt(i) > 122) {
-			return false;
-		}
-	}
-	return true;
-}
-
 function clickReset() {
 	document.getElementById('licensePlate').value = '';
 	document.getElementById('carType').value = '';
@@ -59,15 +27,10 @@ function clickReset() {
 }
 
 function clickUpdate() {
-	if (!checkCarType()) {
-		alert('Car type is invalid');
+	if (!checkLicensePlate()) {
+		alert('License plate is invalid');
 	}
-	if (!checkCarColor()) {
-		alert('Car color is invalid');
-	}
-	else {
-		document.getElementById('form-edit-delete-car').submit();
-	}
+	document.getElementById('form-edit-delete-car').submit();
 }
 
 function clickDetele() {
